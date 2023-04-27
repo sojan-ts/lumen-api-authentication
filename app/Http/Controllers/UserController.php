93fda95aebfd8f0dae13f9a3867f3cb22c0bfa18
Sojan TS <64725062+sojan-ts@users.noreply.github.com>
2023-04-27 17:07:41 +0530

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,5 +13,44 @@ class UserController extends Controller
     {
         return (new ListController())->allUsers();
     }
+    
+     public function insertContent(Request $request) {
+        $model = Content::class;
+        $OperationHelperController = new OperationHelperController();
+        $fieldNames = ['title','url','description','length', 'categories'];
+        $validations = ['required|string', 'required|string',
+        'required|string','required|string','required|integer'];
+        $fieldValues = [
+            'title' => $request->input('title'),
+            'url' => $request->input('url'),
+            'visibility' => 2,
+            'description' => $request->input('description'),
+            'length' => $request->input('length'),
+            'categories' => $request->input('categories'),
+        ];
+        $fieldsToIgnore = ['visibility'];
+        $response = $OperationHelperController->
+            insertValues(
+                $request,
+                $model,
+                $fieldNames,
+                $fieldValues,
+                false,
+                null,
+                null,
+                $validations,
+                $fieldsToIgnore
+            );
+        return $response;
+    }
+
+    public function updateContent(Request $request, $id)
+    {
+        $model = Content::class;
+        $OperationHelperController = new OperationHelperController();
+        $response = $OperationHelperController->
+            updateRecord($request, $id, $model);
+        return $response;
+    }
 
 }
